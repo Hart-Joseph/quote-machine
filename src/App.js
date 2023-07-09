f9a8ca2602ccbdf07a72b64d9c8fefe9b1350f62
Joseph Hart <joseph88hart@protonmail.com>
2023-07-09 00:23:38 -0700

    Create interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box">
+      <div id="text">
+        <p>Quote goes here</p>
+      </div>
+      <div id="author">
+        <p>Author goes here</p>
+      </div>
+      <button id="new-quote">New Quote</button>
+      <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet Quote</a>
     </div>
   );
 }
